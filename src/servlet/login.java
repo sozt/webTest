3c097ed74304cd50d88cd5025f14d4219f01884f
sozt <358003642@qq.com>
2015-03-04 12:10:21 +0800

    merge

---
+++ src/servlet/login.java
@@ -0,0 +1,14 @@
+package servlet;
+
+public class login {
+	public void login(){
+		System.out.println("html");
+		int a = 1+1;
+		int b = a;
+		int n = b-2;
+		System.out.println(n-a+1);
+		String str = "sozt1990";
+		String[] sp = str.split("ozt");
+		System.out.println("done");
+	}
+}
